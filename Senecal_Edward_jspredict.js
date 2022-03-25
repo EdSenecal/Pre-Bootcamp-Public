22e5200991ecffb3e3b77b0e750999d0420ad56d
EdSenecal <102299679+EdSenecal@users.noreply.github.com>
2022-03-25 05:57:58 -0700

    Add files via upload

---
+++ Senecal_Edward_jspredict.js
@@ -0,0 +1,10 @@
+// LIne will print out "I was born in 1980"
+// "" added for clarity
+
+// It should still state the same info.
+//"I was born in 1980"
+
+//Summing Numbers!
+//num1 is 10
+//num2 is 20
+//30
