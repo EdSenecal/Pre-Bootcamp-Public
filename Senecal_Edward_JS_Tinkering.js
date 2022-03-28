53961d196a0858448285b25afa50cda516b9aa71
EdSenecal <102299679+EdSenecal@users.noreply.github.com>
2022-03-28 13:09:11 -0700

    Add files via upload

---
+++ Senecal_Edward_JS_Tinkering.js
@@ -0,0 +1,12 @@
+var childHeight = 58;
+function dislplayIfChildIsAbleToRideTheRollerCoaster(childHeight){
+    if (childHeight > 52){
+        console.log ("Get on that ride, Kiddo!");
+    }
+        
+    else {
+        console.log ("Sorry, kiddo.  Maybe next year.");
+
+    }   
+}
+    console.log (dislplayIfChildIsAbleToRideTheRollerCoaster);
